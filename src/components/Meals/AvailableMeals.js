bc95f8627366217247d93443d58842e15ed8dda7
Ryan Cabanas <ryangithub777@gmail.com>
2022-07-27 00:39:02 -0400

    Probably Lesson 215 (My Own Attempts): Change the `AvailableMeals` component so that it loads initial available meals from Firebase, instead of from static data in the component file.

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,39 @@
+import { useState, useEffect, useCallback } from 'react';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [availableMeals, setAvailableMeals] = useState([]);
+
+  const url =
+    'https://react-http-4bc80-default-rtdb.firebaseio.com/availableMeals.json';
+
+  const fetchMeals = useCallback(async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const dataKeys = Object.keys(data);
+    const fetchedMeals = [];
+
+    for (const key of dataKeys) {
+      const meal = {
+        id: key,
+        name: data[key].name,
+        description: data[key].description,
+        price: data[key].price,
+      };
+      fetchedMeals.push(meal);
+    }
+
+    setAvailableMeals(fetchedMeals);
+  }, []);
+
+  useEffect(() => {
+    fetchMeals();
+  }, [fetchMeals]);
+
+  const mealsList = availableMeals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
